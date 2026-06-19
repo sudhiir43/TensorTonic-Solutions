4bb77c4eaf87c59f7b0aa857beafbb8936d0df33
Sudheer Reddy <sudhiir43@outlook.com>
2026-06-20 02:42:08 +0530

    Solve entropy-node

---
+++ entropy-node/entropy-node.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def entropy_node(y):
+    """
+    Compute entropy for a single node using stable logarithms.
+    """
+    # Write code here
+    entropy = 0
+    keys = set(y)
+
+    for key in keys:
+        p = y.count(key)/len(y)
+        entropy += p*np.log2(p)
+        # print(p, key)
+    return float(-1*entropy)
